2c4742ca15ad12c6499ed5423759190fe6362224
FonKnp <fonnfonk@gmail.com>
2024-06-17 18:38:33 -0400

    commit

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,34 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readwrite');
+    const store = db.objectStore('jate');
+
+    const result = await store.put({ value: content, id: 1 });
+
+    console.log('Data added', result);
+  }  catch (err) {
+    console.error('putDb not implemented');
+  }
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+
+    const result = await store.get(1);
+
+    console.log('Data from database', result);
+    return result?.value;
+  } catch (err) {
+    console.error('getDb not implemented');
+  }
+}
 
 initdb();
